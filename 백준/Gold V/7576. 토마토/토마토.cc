748b625d2292bf9456cc4d05ed5ceea702b7480b
woojinrnd <122770475+woojinrnd@users.noreply.github.com>
2024-02-15 21:30:26 +0900

    [Gold V] Title: 토마토, Time: 88 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Gold V/7576. 토마토/토마토.cc
+++ 백준/Gold V/7576. 토마토/토마토.cc
@@ -1,59 +1,77 @@
 #include <iostream>
 #include <queue>
-#include <algorithm>
 
 using namespace std;
 
-struct Coord {
+int n, m;
+int N[1000][1000];
+int dist[1000][1000];
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+struct point {
 	int x, y;
 };
-
-int n, m;
-int N[1002][1002];  // 입력으로 받는 토마토 상자의 상태를 저장하는 배열
-int dist[1002][1002];  // 각 토마토까지의 최단 거리를 저장하는 배열
-int dx[4] = {1, 0, -1, 0};  // x 방향 이동에 대한 배열
-int dy[4] = {0, 1, 0, -1};  // y 방향 이동에 대한 배열
+queue<point> q; 
 
 int main() {
-	ios::sync_with_stdio(0);  // 동기화를 통해 입출력 속도 향상
-	cin.tie(0);  // 입출력 버퍼를 비우지 않도록 함
-	cin >> m >> n;  // 가로, 세로 크기 입력
-	queue<Coord> Q;  // BFS를 위한 큐
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < m; ++j) {
-			cin >> N[i][j];  // 토마토 상태 입력
-			if (N[i][j] == 1)  // 익은 토마토라면 큐에 좌표를 넣음
-				Q.push({i, j});
-			if (N[i][j] == 0)  // 익지 않은 토마토라면 최단 거리를 나타내는 dist 배열을 -1로 초기화
-				dist[i][j] = -1;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> m >> n;
+	for (int i=0; i<n; ++i) {
+		for (int j=0; j<m; ++j) {
 		}
 	}
-	
-	while (!Q.empty()) {
-		Coord cur = Q.front();  // 현재 위치를 큐에서 꺼냄
-		Q.pop();
-		for (int dir = 0; dir < 4; ++dir) {
+	for (int i=0; i<n; ++i) {
+		for (int j=0; j<m; ++j) {
+			cin >> N[i][j];
+			// 익은 토마토(1) -> bfs 시작점
+			if (N[i][j] == 1) {
+				dist[i][j] = 0;
+				q.push({i, j}); 
+			}
+			// 탐색 필요 구간 
+			if (N[i][j] == 0) {
+				dist[i][j] = -1;
+			}
+		}
+	}
+	// dist 배열 -> 일수 계산 
+	while(!q.empty()) {
+		point cur = q.front();
+		q.pop();
+		for (int dir=0; dir<4; ++dir) {
 			int nx = cur.x + dx[dir];
 			int ny = cur.y + dy[dir];
-			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;  // 범위를 벗어나면 스킵
-			if (dist[nx][ny] >= 0) continue;  // 이미 방문한 곳이면 스킵
-			dist[nx][ny] = dist[cur.x][cur.y] + 1;  // 최단 거리 갱신
-			Q.push({nx, ny});  // 다음 위치를 큐에 넣음
+			if (nx<0||nx>=n||ny<0||ny>=m) continue;
+			if (dist[nx][ny]>=0||N[nx][ny]==-1) continue;
+			dist[nx][ny] = dist[cur.x][cur.y] + 1;
+			q.push({nx, ny});
 		}
 	}
 	
-	int ans = 0;
-	
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < m; ++j) {
-			if (dist[i][j] == -1) {  // 익지 않은 토마토가 있는 경우
-				cout << -1;
-				return 0;
+	// 최종 일수 계산
+	// 익지 않은 토마토가 하나라도 있다면 -> -1 출력
+	// 다 익었다면 						  ->  0 출력 
+	int res = 0;
+	int max_day = 0;
+	for (int i=0; i<n; ++i) {
+		for (int j=0; j<m; ++j) {
+			// 예외 처리 -> 안쪽 for문 탈출
+			if (dist[i][j] == -1) {
+				res = -1; 
+				break;
 			}
-			ans = max(ans, dist[i][j]);  // 최단 거리 중 가장 큰 값 찾기
+			else {
+				if (dist[i][j] > max_day) {
+					max_day = dist[i][j];
+					res = max_day;
+				}
+			}
 		}
+		// 예외 처리 -> 바깥쪽 for문 탈출 
+		if (res == -1) break;
 	}
-	cout << ans;  // 결과 출력
+	cout << res;	
 	
 	return 0;
 }
