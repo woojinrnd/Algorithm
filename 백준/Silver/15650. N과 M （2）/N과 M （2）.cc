4c08c9d6e9faad6d77d4c9d8020bc3620922f925
woojinrnd <122770475+woojinrnd@users.noreply.github.com>
2024-02-26 23:27:02 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -1,41 +1,37 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int n, m;
-vector<int> nums;
-vector<int> combination;
+int N[10];
+int isused[10];
+
+void func(int k);
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cin >> n >> m;
-    // 입력 집합
-    for (int i=0; i<n; ++i) {
-    	nums.push_back(i+1);
-	}
+	func(0);
 	
-    // 선택할 요소의 개수
-    int k = m;
+	return 0;
+}
 
-    // 조합을 저장할 벡터
-    vector<int> combination(nums.size(), 0);
-    for (int i = 0; i < k; ++i) {
-        combination[i] = 1;
-    }
-
-    // prev_permutation을 이용하여 이전 조합을 탐색하고 출력
-    do {
-        // 현재 조합 출력
-        for (int i = 0; i < nums.size(); ++i) {
-            if (combination[i] == 1) {
-                cout << nums[i] << " ";
-            }
-        }
-        cout << endl;
-    } while (prev_permutation(combination.begin(), combination.end()));
-
-    return 0;
+void func(int k) {
+	if (k == m) {
+		for (int i=0; i<m; ++i) {
+			cout << N[i] << ' ';
+		}
+		cout << '\n';
+	}
+	int st = 1;
+	if (k != 0) st = N[k-1] + 1; // 이전 지점의 +1  
+	for (int i=st; i<=n; ++i) {
+		if (!isused[i]) {
+			isused[i] = 1;
+			N[k] = i;
+			func(k+1);
+			isused[i] = 0;
+		}
+	}
 }
