6a6ae75f83478acbed3cd7f94e163466f8e7d422
woojinrnd <122770475+woojinrnd@users.noreply.github.com>
2024-04-18 19:28:37 +0900

    [Silver II] Title: 키로거, Time: 256 ms, Memory: 19384 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.cc
+++ 백준/Silver/5397. 키로거/키로거.cc
@@ -1,53 +1,46 @@
 #include <iostream>
 #include <list>
 #include <string>
+
 using namespace std;
 
+int Tc;
+
 int main() {
-    // C++ 표준 입출력과 동기화를 끊어줌으로써 입출력 성능 향상
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    // 테스트 케이스의 개수 입력
-    int t;
-    cin >> t;
-
-    // 테스트 케이스 반복
-    while (t--) {
-        // 문자열을 저장할 리스트 및 iterator 초기화
-        list<char> lis;
-        list<char>::iterator iter = lis.begin();
-        
-        // 문자열 입력
-        string s;
-        cin >> s;
-
-        // 입력된 문자열 순회
-        for (auto c : s) {
-            // '-'인 경우 현재 iterator의 이전 원소 삭제
-            if (c == '-') {
-                if (iter != lis.begin())
-                    iter = lis.erase(--iter);
-            }
-            // '<'인 경우 iterator를 이전으로 이동
-            else if (c == '<') {
-                if (iter != lis.begin())
-                    iter--;
-            }
-            // '>'인 경우 iterator를 다음으로 이동
-            else if (c == '>') {
-                if (iter != lis.end())
-                    iter++;
-            }
-            // 그 외의 경우 현재 iterator 위치에 문자 추가
-            else
-                lis.insert(iter, c);
-        }
-
-        // 결과 출력
-        for (auto a : lis)
-            cout << a;
-        cout << '\n';
-    }
-    return 0;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> Tc;
+	while(Tc--) {
+		string s;
+		cin >> s;
+		list<char> L;
+		list<char>::iterator cursor = L.begin();
+		for (auto c : s) {
+			if (c == '<') {
+				if (cursor != L.begin()) {
+					--cursor;
+				}
+			}
+			else if (c == '>') {
+				if (cursor != L.end()) {
+					++cursor;
+				}
+			}
+			else if (c == '-') {
+				if (cursor != L.begin()) {
+					--cursor;
+					cursor = L.erase(cursor);
+				}
+			}
+			else {
+				L.insert(cursor, c);
+			}
+		}	
+		for (auto i : L) {
+			cout << i;
+		}
+		cout << '\n';
+	}
+	
+	return 0;
 }
