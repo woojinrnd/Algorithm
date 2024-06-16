b1438187c767f454fb59589038b9f4f667f54a8c
woojinrnd <122770475+woojinrnd@users.noreply.github.com>
2024-06-16 21:18:49 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,32 +1,41 @@
 #include <iostream>
-#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int n;
-int N[1000][1000];
-int dp[1000][1000];
+int n, ans;
+int N[1003][3];
+int dp[1003][3];
 
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+void input() {
 	cin >> n;
-	// cost matrix
-	for (int i=0; i<n; ++i) {
-		for (int j=0; j<3; ++j) {
-			cin >> N[i][j];
-			dp[0][j] = N[0][j];
-		}
+	for (int i=1; i<=n; ++i) {
+		cin >> N[i][0] >> N[i][1] >> N[i][2];
 	}
-	// minimum sum cost matrix
-    // row(i) -> row(i+1) : row(i)에서 동일 열을 제외한 최솟값 찾기
-    // dp[i][j] = min(dp[i-1][j-1], dp[i-1][j+1])
-	for (int i=1; i<n; ++i) {
+	
+	dp[0][0] = dp[0][1] = dp[0][2] = 0;
+	N[0][0] = N[0][1] = N[0][2] = 0;
+}
+
+void solution() {
+	for (int i=1; i<=n; ++i) {
 		dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + N[i][0];
 		dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + N[i][1];
 		dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + N[i][2];
 	}
-	cout << *min_element(dp[n-1], dp[n-1]+3);
+	ans = min(min(dp[n][0], dp[n][1]), dp[n][2]);
+	cout << ans;
+}
+
+void solve() {
+	input();
+	solution();
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	solve();
 	
 	return 0;
 }
