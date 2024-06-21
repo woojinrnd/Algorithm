a1f10479ffacb6e179bb8bd7e4a66e354203e29d
woojinrnd <122770475+woojinrnd@users.noreply.github.com>
2024-06-21 18:04:48 +0900

    [Silver III] Title: 두 수의 합, Time: 8 ms, Memory: 4756 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -1,35 +1,28 @@
-/*
-하나는 원소 체크(기록)을 해서 배열의 원소 
-a에 대해서 x-a가 배열에 있는 지 체크하는 법
-
-또 하나는 정렬 후 투포인터를 이용하는 법.
-*/
-
 #include <iostream>
-#include <algorithm>
+
 using namespace std;
 
-int n, x, tmp;
-int num[100001];
-int cnt[2000001]; 
-
+int n;
+int arr[100005];
+int dif[100005];
+bool check[2000005];
+int sum;
+int res;
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cin >> n;
 	for (int i=0; i<n; ++i) {
-		cin >> tmp;
-		num[i] = tmp;
-		cnt[tmp] = 1;
+		cin >> arr[i];
+		check[arr[i]] = true;
 	}
-	cin >> x;
-	int sum =0;
+	cin >> sum;
 	for (int i=0; i<n; ++i) {
-		if (x-num[i]>0 && cnt[x-num[i]] != 0 && x-num[i]>num[i])
-			sum+=1;
+		if (sum - arr[i] > 0 && check[sum-arr[i]] != 0 && sum - arr[i] > arr[i])
+		res++;			
 	}
-	cout << sum;
-
+	cout << res;
+	
 	return 0;
 }
