0ae17eb6004b966c70a97399309aaa153b749587
woojinrnd <122770475+woojinrnd@users.noreply.github.com>
2024-07-15 19:49:11 +0900

    [Gold V] Title: 토마토, Time: 96 ms, Memory: 11428 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.cc
+++ 백준/Gold/7569. 토마토/토마토.cc
@@ -1,73 +1,82 @@
 #include <iostream>
 #include <queue>
-
 using namespace std;
 
-const int max_size = 102;
-
-int h, n, m; //[h][n][m] = [높이][세로][가로]
-int N[max_size][max_size][max_size];
-int dist[max_size][max_size][max_size];
-int dx[] = {1, -1, 0, 0, 0, 0};
-int dy[] = {0, 0, 1, -1, 0, 0};
+int h, n, m; // 높이 세로 가로
+int N[100][100][100];
+int dist[100][100][100];
+int dx[] = {1, 0, -1, 0, 0, 0};
+int dy[] = {0, 1, 0, -1, 0, 0};
 int dz[] = {0, 0, 0, 0, 1, -1};
 struct point {
-	int x, y, z;
+    int x, y, z;
 };
+int day;
+queue<point> q;
 
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	queue<point> Q;
-	cin >> m >> n >> h;
-
-	
-	for (int k=0; k<h; ++k) {
-		for (int i=0; i<n; ++i) {
-			for (int j=0; j<m; ++j) { 
-				cin >> N[k][i][j];
-				// 익은 토마토 : BFS시작점 
-				if (N[k][i][j] == 1) {
-					Q.push({i,j,k});
-				}
-				// 익지 않은 토마토 : 아직 방문하지 않음
-				// bfs 돌릴 탐색 구간 
-				if (N[k][i][j] == 0) {
-					dist[k][i][j] = -1;
-				} 
-				
-			}
-		}
-	}
-	while (!Q.empty()) {
-		point cur = Q.front();
-		Q.pop();
-		for (int dir=0; dir<6;++dir) {
-			int nx = cur.x + dx[dir];
-			int ny = cur.y + dy[dir];
-			int nz = cur.z + dz[dir];
-			if (nx<0||nx>=n||ny<0||ny>=m||nz<0||nz>=h) continue;
-			if (dist[nz][nx][ny]>=0||N[nz][nx][ny]==-1) continue;
-			dist[nz][nx][ny] = dist[cur.z][cur.x][cur.y] + 1;
-			Q.push({nx, ny, nz});
-		}
-	}
-	
-    int ans = 0;
-    for (int k=0; k<h; ++k) {
-        for (int i=0; i<n; ++i) {
-            for (int j=0; j<m; ++j) {
-                if (N[k][i][j] == 0 && dist[k][i][j] == -1) {
-                    cout << -1;
-                    return 0;
+void input() {
+    cin >> m >> n >> h;
+    for (int i=0; i<h; ++i) {
+        for (int j=0; j<n; ++j) {
+            for (int k=0; k<m; ++k) {
+                cin >> N[i][j][k];
+                if (N[i][j][k]==1) {
+                    q.push({j, k, i});
+                    dist[i][j][k]=0;
                 }
-                if (ans < dist[k][i][j]) ans = dist[k][i][j];
+                else if (N[i][j][k]==0) {
+                    dist[i][j][k] = -1;
+                }
             }
         }
     }
-    cout << ans;
-	
-	
-	
-	return 0;
+}
+
+void solution() {
+    while(!q.empty()) {
+        point cur = q.front();
+        q.pop();
+
+        for (int dir=0; dir<6; ++dir) {
+            int nx = cur.x + dx[dir];
+            int ny = cur.y + dy[dir];
+            int nz = cur.z + dz[dir];
+            if (nx<0||nx>=n||ny<0||ny>=m||nz<0||nz>=h) continue;
+            if (dist[nz][nx][ny]>=0||N[nz][nx][ny]==-1) continue;
+            dist[nz][nx][ny] = dist[cur.z][cur.x][cur.y] + 1;
+            q.push({nx, ny, nz});
+        }
+    }
+}
+
+void calc_day() {
+    for (int i=0; i<h; ++i) {
+        for (int j=0; j<n; ++j) {
+            for (int k=0; k<m; ++k) {
+                if (N[i][j][k]==0 && dist[i][j][k]==-1) {
+                    day = -1;
+                    return ;
+                }
+                day = max(day, dist[i][j][k]);
+            }
+        }
+    }
+}
+
+
+void solve() {
+    input();
+    solution();
+    calc_day();
+    cout << day << '\n';
+}
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    solve();
+
+    return 0;
 }
