01bf54cf480d00e2d7bb4fc7323de7f6478d9f32
woojinrnd <122770475+woojinrnd@users.noreply.github.com>
2024-07-15 17:58:40 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -3,40 +3,51 @@
 
 using namespace std;
 
-char N[101][101];
-bool vis[101][101];
 int n;
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
-
+char N[103][103];
+bool vist[103][103];
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
 struct point {
     int x, y;
 };
+int ans1, ans2;
 
-// 좌표가 같은 색상인 영역을 BFS로 탐색하는 함수
-void bfs(int i, int j) {
-    queue<point> Q;
-    Q.push({i, j});
-    while (!Q.empty()) {
-        point cur = Q.front();
-        Q.pop();
-        for (int dir = 0; dir < 4; ++dir) {
-            int nx = cur.x + dx[dir];
-            int ny = cur.y + dy[dir];
-            if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-            if (vis[nx][ny] == 1 || N[i][j] != N[nx][ny]) continue;
-            vis[nx][ny] = 1;
-            Q.push({nx, ny});
+void input() {
+    cin >> n;
+    for (int i=0; i<n; ++i) {
+        for (int j=0; j<n; ++j) {
+            cin >> N[i][j];
         }
     }
 }
 
-// 구역 수를 세는 함수 (이름 변경)
-int countAreas() {
+void bfs(int x, int y) {
+    queue<point> q;
+    vist[x][y] = true;
+    q.push({x, y});
+
+    while(!q.empty()) {
+        point cur = q.front();
+        q.pop();
+
+        for (int dir=0; dir<4; ++dir) {
+            int nx = cur.x + dx[dir];
+            int ny = cur.y + dy[dir];
+            if (nx<0||nx>=n||ny<0||ny>=n) continue;
+            if (N[nx][ny]!=N[cur.x][cur.y]||vist[nx][ny]) continue;
+            vist[nx][ny] = true;
+            q.push({nx, ny});
+        }
+    }
+}
+
+int countArea() {
     int cnt = 0;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if (!vis[i][j]) {
+    for (int i=0; i<n; ++i) {
+        for (int j=0; j<n; ++j) {
+            if (!vist[i][j]) {
+                vist[i][j] = true;
                 cnt++;
                 bfs(i, j);
             }
@@ -45,41 +56,31 @@ int countAreas() {
     return cnt;
 }
 
-int main() {
+void solution() {
+    ans1 = countArea(); // 정상
+    for (int i=0; i<n; ++i) {
+        for (int j=0; j<n; ++j) {
+            vist[i][j] = false;
+            if (N[i][j] == 'G') N[i][j] = 'R';
+        }
+    }
+    ans2 = countArea();
+
+    cout << ans1 << ' ' << ans2 << '\n';
+}
+
+void solve() {
+    input();
+    solution();
+}
+
+int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
+    cout.tie(0);
 
-    cin >> n;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            cin >> N[i][j];
-        }
-    }
-
-    // 초기화
-    for (int i = 0; i < n; ++i) {
-        fill(vis[i], vis[i] + n, false);
-    }
-
-    int not_g = countAreas(); // 적록색약 x
-
-    // 초기화
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if (N[i][j] == 'G') {
-                N[i][j] = 'R';
-            }
-        }
-    }
-
-    // 초기화
-    for (int i = 0; i < n; ++i) {
-        fill(vis[i], vis[i] + n, false);
-    }
-
-    int is_g = countAreas();
-
-    cout << not_g << " " << is_g;
+    solve();
 
     return 0;
 }
+
