3597263032879cbcb9f98d62ee2497527513b914
woojinrnd <122770475+woojinrnd@users.noreply.github.com>
2025-06-09 22:17:09 +0900

    [Gold III] Title: 불!, Time: 44 ms, Memory: 10980 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -4,81 +4,84 @@
 using namespace std;
 
 int n, m;
-char N[1003][1003];
-int dist1[1003][1003]; // FIRE
-int dist2[1003][1003]; // JIHOON
+char N[1002][1002];
+int dist1[1002][1002]; // fire
+int dist2[1002][1002]; // jihoon
 int dx[] = {1, 0, -1, 0};
 int dy[] = {0, 1, 0, -1};
-struct point{
-	int x, y;
+struct point {
+    int x, y;
 };
+queue<point> q1; // fire
+queue<point> q2; // jihoon
+int res;
 
-queue<point> q1; // FIRE
-queue<point> q2; // JIHOON
+void input() {
+    cin >> n >> m;
+    for (int i=0; i<n; ++i) {
+        for (int j=0; j<m; ++j) {
+            cin >> N[i][j];
+            if (N[i][j] == 'F') {
+                dist1[i][j] = 1;
+                q1.push({i, j});
+            }
+            else if (N[i][j] == 'J') {
+                dist2[i][j] = 1;
+                q2.push({i, j});
+            }
+        }
+    }
+}
 
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> n >> m;
-	// Initialize dist array
-	for (int i=0; i<n; ++i) {
-		for (int j=0; j<m; ++j) {
-			dist1[i][j] = -1;
-			dist2[i][j] = -1;
-		}
-	}
-	
-	for (int i=0; i<n; ++i) {
-		for (int j=0; j<m; ++j) {
-			cin >> N[i][j];
-			// BFS start point
-			// Fire
-			if (N[i][j] == 'F') {
-				dist1[i][j] = 0;
-				q1.push({i, j});
-			}
-			// JIHOON
-			if (N[i][j] == 'J') {
-				dist2[i][j] = 0;
-				q2.push({i, j});
-			}
-		}
-	}
-	// BFS - Fire
-	while(!q1.empty()) {
-		point cur = q1.front();
-		q1.pop();
-		for (int dir=0; dir<4; ++dir) {
-			int nx = cur.x + dx[dir];
-			int ny = cur.y + dy[dir];
-			if (nx<0||nx>=n||ny<0|ny>=m) continue;
-			if (N[nx][ny]=='#'||dist1[nx][ny]>=0) continue;
-			dist1[nx][ny] = dist1[cur.x][cur.y]+1;
-			q1.push({nx, ny});
-		} 
-	}
-	// BFS - Jihoon
-	while(!q2.empty()) {
-		point cur = q2.front();
-		q2.pop();
-		for (int dir=0; dir<4; ++dir) {
-			int nx = cur.x + dx[dir];
-			int ny = cur.y + dy[dir];
-			// 탈출 조건 
-			if (nx<0||nx>=n||ny<0||ny>=m) {
-				cout << dist2[cur.x][cur.y]+1 << '\n';
-				return 0;
-			}
-			if (N[nx][ny]=='#'|| dist2[nx][ny]>=0) continue; 
-			// 불이 지훈이보다 먼저 간 경우
-			// 불의 (다음좌표)와 지훈이의 (현재좌표)를 비교
-			// 다음 좌표를 큐에 담아 bfs돌릴지 말지를 결정할거니까 
-			// dist1[nx][ny] <=  dist2[cur.x][cur.y]+1
-			if (dist1[nx][ny]!=-1 && dist1[nx][ny]<=dist2[cur.x][cur.y]+1) continue;
-			dist2[nx][ny] = dist2[cur.x][cur.y]+1;
-			q2.push({nx, ny});
-		}
-	}
-	cout << "IMPOSSIBLE";
-	return 0;
+void solution() {
+    // FIRE BFS
+    while(!q1.empty()) {
+        point cur = q1.front();
+        q1.pop();
+        for (int dir=0; dir<4; ++dir) {
+            int nx = cur.x + dx[dir];
+            int ny = cur.y + dy[dir];
+            if (nx<0||nx>=n||ny<0||ny>=m) continue;
+            if (N[nx][ny]=='#'||dist1[nx][ny]>0) continue;
+            dist1[nx][ny] = dist1[cur.x][cur.y]+1;
+            q1.push({nx, ny});
+        }
+    }
+
+    // JIHOON BFS
+    while(!q2.empty()) {
+        point cur = q2.front();
+        q2.pop();
+        for (int dir=0; dir<4; ++dir) {
+            int nx = cur.x + dx[dir];
+            int ny = cur.y + dy[dir];
+            // 탈출
+            if (nx<0||nx>=n||ny<0||ny>=m) {
+                res = dist2[cur.x][cur.y];
+                cout << res;
+                return ;
+            }
+            if (N[nx][ny]=='#'||dist2[nx][ny]>0) continue;
+            if (dist1[nx][ny]>0 && dist1[nx][ny]<=dist2[cur.x][cur.y]+1) continue;
+            dist2[nx][ny] = dist2[cur.x][cur.y] + 1;
+            q2.push({nx, ny});
+        }
+    }
+
+    cout << "IMPOSSIBLE";
+}
+
+void solve() {
+    input();
+    solution();
+}
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    solve();
+
+    return 0;
 }
