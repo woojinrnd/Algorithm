db84d71f7982a1295a9ed3ad8234f5dd5c40a8ca
woojinrnd <122770475+woojinrnd@users.noreply.github.com>
2025-06-14 14:06:03 +0900

    [Gold III] Title: 불!, Time: 36 ms, Memory: 10980 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -3,71 +3,68 @@
 
 using namespace std;
 
-int n, m;
+int r, c;
 char N[1002][1002];
-int dist1[1002][1002]; // fire
-int dist2[1002][1002]; // jihoon
+int dist1[1002][1002]; // FIRE
+int dist2[1002][1002]; // JIHOON
 int dx[] = {1, 0, -1, 0};
 int dy[] = {0, 1, 0, -1};
 struct point {
     int x, y;
 };
-queue<point> q1; // fire
-queue<point> q2; // jihoon
-int res;
+queue<point> F;
+queue<point> J;
 
 void input() {
-    cin >> n >> m;
-    for (int i=0; i<n; ++i) {
-        for (int j=0; j<m; ++j) {
+    cin >> r >> c;
+    for (int i=0; i<r; ++i) {
+        for (int j=0; j<c; ++j) {
             cin >> N[i][j];
             if (N[i][j] == 'F') {
                 dist1[i][j] = 1;
-                q1.push({i, j});
+                F.push({i, j});
             }
             else if (N[i][j] == 'J') {
                 dist2[i][j] = 1;
-                q2.push({i, j});
+                J.push({i, j});
             }
         }
     }
 }
 
 void solution() {
-    // FIRE BFS
-    while(!q1.empty()) {
-        point cur = q1.front();
-        q1.pop();
+    while(!F.empty()) {
+        point cur = F.front();
+        F.pop();
+
         for (int dir=0; dir<4; ++dir) {
             int nx = cur.x + dx[dir];
             int ny = cur.y + dy[dir];
-            if (nx<0||nx>=n||ny<0||ny>=m) continue;
+            if (nx<0||nx>=r||ny<0||ny>=c) continue;
             if (N[nx][ny]=='#'||dist1[nx][ny]>0) continue;
             dist1[nx][ny] = dist1[cur.x][cur.y]+1;
-            q1.push({nx, ny});
+            F.push({nx, ny});
         }
     }
 
-    // JIHOON BFS
-    while(!q2.empty()) {
-        point cur = q2.front();
-        q2.pop();
+    while(!J.empty()) {
+        point cur = J.front();
+        J.pop();
+
         for (int dir=0; dir<4; ++dir) {
             int nx = cur.x + dx[dir];
             int ny = cur.y + dy[dir];
-            // 탈출
-            if (nx<0||nx>=n||ny<0||ny>=m) {
-                res = dist2[cur.x][cur.y];
-                cout << res;
+            // Escape
+            if (nx<0||nx>=r||ny<0||ny>=c) {
+                cout << dist2[cur.x][cur.y];
                 return ;
             }
             if (N[nx][ny]=='#'||dist2[nx][ny]>0) continue;
             if (dist1[nx][ny]>0 && dist1[nx][ny]<=dist2[cur.x][cur.y]+1) continue;
             dist2[nx][ny] = dist2[cur.x][cur.y] + 1;
-            q2.push({nx, ny});
+            J.push({nx, ny});
         }
     }
-
     cout << "IMPOSSIBLE";
 }
 
@@ -85,3 +82,4 @@ int main(void) {
 
     return 0;
 }
+
