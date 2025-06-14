dc01f039ebb3489960421ce72478b75da9fb3071
woojinrnd <122770475+woojinrnd@users.noreply.github.com>
2025-06-14 15:25:58 +0900

    [Silver I] Title: 나이트의 이동, Time: 20 ms, Memory: 2592 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
@@ -4,50 +4,72 @@
 
 using namespace std;
 
-int T; // test case 
-int n; // 체스판 길이
-int vist[304][304]; // 각 칸까지의 최소 이동 횟수를 저장하는 배열
-int dx[] = {2, 2, -2, -2, 1, 1, -1, -1}; // 이동할 수 있는 모든 경우의 수
-int dy[] = {1, -1, 1, -1, 2, -2, 2, -2};
-
+int Tc, n;
+int N[300][300];
+bool vist[300][300];
+int dx[] = {1, 1, -1, -1, 2, 2, -2, -2};
+int dy[] = {2, -2, 2, -2, 1, -1, 1, -1};
 struct point {
-	int x, y;
+    int x, y;
+    int time;
 };
+point start, finish;
 
-point start, End;
-queue<point> Q;
+void init() {
+    start = point({0, 0});
+    finish = point({0, 0});
+    memset(vist, false, sizeof(vist));
+}
 
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> T;
-	while(T--) {
-		cin >> n;
-		for (int i=0; i<n; ++i) {
-			for (int j=0; j<n; ++j) {
-				vist[i][j] = -1; // 초기화: 방문하지 않은 칸은 -1로 설정
-			}
-		}
-		cin >> start.x >> start.y;
-		vist[start.x][start.y] = 0; // 시작점은 이동 횟수가 0
-		Q.push({start.x, start.y}); // 시작점을 큐에 삽입
+void input() {
+    cin >> n;
+    cin >> start.x >> start.y;
+    cin >> finish.x >> finish.y;
+}
 
-		cin >> End.x >> End.y;
-		
-		while (!Q.empty()) {
-			point cur = Q.front();
-			Q.pop();
-			for (int dir=0; dir<8; ++dir) {
-				int nx = cur.x + dx[dir];
-				int ny = cur.y + dy[dir];
-				if (nx<0 || nx>=n || ny<0 || ny>=n) continue; // 범위를 벗어나면 무시
-				if (vist[nx][ny] >= 0) continue; // 이미 방문한 칸이면 무시
-				vist[nx][ny] = vist[cur.x][cur.y] + 1; // 이동 횟수 갱신
-				Q.push({nx, ny}); // 다음 위치를 큐에 삽입
-			}
-		} 
-		cout << vist[End.x][End.y] << '\n'; // 목적지까지의 최소 이동 횟수 출력
-	}
-	
-	return 0;
+void bfs(int x, int y) {
+    queue<point> q;
+    vist[x][y] = true;
+    q.push({x, y, 0});
+
+    while(!q.empty()) {
+        point cur = q.front();
+        q.pop();
+
+        for (int dir=0; dir<8; ++dir) {
+            int nx = cur.x + dx[dir];
+            int ny = cur.y + dy[dir];
+            if (cur.x == finish.x && cur.y == finish.y) {
+                cout << cur.time << '\n';
+                return ;
+            }
+            if (nx<0||nx>=n||ny<0||ny>=n) continue;
+            if (vist[nx][ny]) continue;
+            vist[nx][ny] = true;
+            q.push({nx, ny, cur.time+1});
+        }
+    }
+}
+
+void solution() {
+    bfs(start.x, start.y);
+}
+
+void solve() {
+    cin >> Tc;
+    for (int i=0; i<Tc; ++i) {
+        init();
+        input();
+        solution();
+    }
+}
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    solve();
+
+    return 0;
 }
