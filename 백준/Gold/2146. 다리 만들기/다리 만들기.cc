7503f88ba42579c370bea88fe1682622cac3c91d
woojinrnd <122770475+woojinrnd@users.noreply.github.com>
2025-06-17 19:45:56 +0900

    [Gold III] Title: 다리 만들기, Time: 32 ms, Memory: 2140 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.cc
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.cc
@@ -1,128 +1,144 @@
-/*
-1. 육지와 바다를 구분한다.
-1-1. 육지와 육지를 구분한다. (인데스 부여)
-1-2. 육지와 바다와 맞닿아 있는 칸을 구한다.(후보군)
-
-2. 육지와 육지간 바다의 거리의 구한다.
-2-1 후보군 * 후보군의 거리를 1:1 매칭하여 거리를 모두 구한다. (BFS)
-
-3. 거리의 최솟값을 구한다.
-*/
-
 #include <iostream>
 #include <queue>
-#include <vector>
 #include <cstring>
-#include <algorithm>
 
 using namespace std;
 
 int n;
-int N[101][101];
-bool vist[101][101];
+int N[100][100][2];
+int dist[100][100];
 int dx[] = {1, 0, -1, 0};
 int dy[] = {0, 1, 0, -1};
 struct point {
     int x, y;
 };
+int area;
 
 void input() {
     cin >> n;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            cin >> N[i][j];
-            if (N[i][j] == 1) N[i][j] = -1;
+    for (int i=0; i<n; ++i) {
+        for (int j=0; j<n; ++j) {
+            cin >> N[i][j][0];
+        }
+    }
+}
+
+void initialize() {
+    for (int i=0; i<n; ++i) {
+        for (int j=0; j<n; ++j) {
+            dist[i][j] = 0;
         }
     }
 }
 
 // 육지 라벨링
-void Labeling_Land(int a, int b, int index) {
-    queue<point> q;
-    N[a][b] = index;
-    vist[a][b] = true;
-    q.push({a, b});
-
-    while (!q.empty()) {
-        point cur = q.front();
-        q.pop();
-
-        for (int dir = 0; dir < 4; ++dir) {
-            int nx = cur.x + dx[dir];
-            int ny = cur.y + dy[dir];
-            if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-            if (vist[nx][ny] || N[nx][ny] != -1) continue;
-            vist[nx][ny] = true;
-            N[nx][ny] = index;
-            q.push({nx, ny});
+void FillArea() {
+    // bfs start (i, j)
+    for (int i=0; i<n; ++i) {
+        for (int j=0; j<n; ++j) {
+            if (N[i][j][0]==0||dist[i][j]!=0) continue;
+            queue<point> q;
+            dist[i][j] = 1;
+            q.push({i, j});
+            area++;
+            N[i][j][1] = area;
+            while(!q.empty()) {
+                point cur = q.front();
+                q.pop();
+                for (int dir=0; dir<4; ++dir) {
+                    int nx = cur.x + dx[dir];
+                    int ny = cur.y + dy[dir];
+                    if (nx<0||nx>=n||ny<0||ny>=n) continue;
+                    if (N[nx][ny][0]==0||dist[nx][ny]!=0) continue;
+                    dist[nx][ny] = 1;
+                    N[nx][ny][1] = area;
+                    q.push({nx, ny});
+                }
+            }
         }
     }
 }
 
 // 섬들 사이의 최단 거리 찾기
-int bfs(int num) {
-    int res = 0;
+// 1. 육지에서 바다와 인접한 요소 찾기
+// 2. A육지 B육지 최소 거리 구하기
+// 3. 반복 횟수 : 육지 라벨 수
+int bfs_to_other_island(int label) {
     queue<point> q;
-    memset(vist, 0, sizeof(vist));  // 방문 배열 초기화
+    int local_dist[100][100];
+    memset(local_dist, -1, sizeof(local_dist));
 
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if (N[i][j] == num) {
-                vist[i][j] = true;
-                q.push({i, j});
-            }
-        }
-    }
-
-    while (!q.empty()) {
-        int size = q.size();
-        while (size--) {
-            point cur = q.front();
-            q.pop();
-
-            for (int dir = 0; dir < 4; ++dir) {
-                int nx = cur.x + dx[dir];
-                int ny = cur.y + dy[dir];
-                if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-                if (N[nx][ny] != 0 && N[nx][ny] != num) return res;  // 다른 섬을 만나면 현재 거리 반환
-                else if (N[nx][ny] == 0 && !vist[nx][ny]) {
-                    vist[nx][ny] = true;
-                    q.push({nx, ny});
+    // 1. 현재 섬의 가장자리만 큐에 넣기
+    for (int i=0; i<n; ++i) {
+        for (int j=0; j<n; ++j) {
+            if (N[i][j][1] == label) {
+                for (int dir=0; dir<4; ++dir) {
+                    int nx = i + dx[dir];
+                    int ny = j + dy[dir];
+                    if (nx<0||nx>=n||ny<0||ny>=n) continue;
+                    if (N[nx][ny][0] == 0) {
+                        local_dist[i][j] = 0;
+                        q.push({i, j});
+                        break;
+                    }
                 }
             }
         }
-        res++;
     }
 
-    return res;
-}
+    // 2. BFS로 바다 확장
+    while(!q.empty()) {
+        point cur = q.front();
+        q.pop();
 
-void solution() {
-    int land_label = 1;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if (N[i][j] == -1) {
-                Labeling_Land(i, j, land_label);
-                land_label++;
+        for (int dir=0; dir<4; ++dir) {
+            int nx = cur.x + dx[dir];
+            int ny = cur.y + dy[dir];
+            if (nx<0||nx>=n||ny<0||ny>=n) continue;
+            if (N[nx][ny][1] == label) continue;
+            if (local_dist[nx][ny]!=-1) continue;
+
+            if (N[nx][ny][0] == 0) {
+                local_dist[nx][ny] = local_dist[cur.x][cur.y] + 1;
+                q.push({nx, ny});
+            }
+            else {
+                return local_dist[cur.x][cur.y];
             }
         }
     }
-
-    int answer = 9999;
-    for (int i = 1; i < land_label; ++i) {
-        answer = min(answer, bfs(i));
-    }
-
-    cout << answer << '\n';
+    return 1e9;
 }
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+void show() {
+    for (int i=0; i<n; ++i) {
+        for (int j=0; j<n; ++j) {
+            cout << N[i][j][1] << ' ';
+        }
+        cout << '\n';
+    }
+}
 
+void solution() {
+    FillArea();
+    int ans = 1e9;
+    for (int i=1; i<=area; ++i) {
+        ans = min(ans, bfs_to_other_island(i));
+    }
+
+    cout << ans << '\n';
+}
+
+void solve() {
     input();
     solution();
+}
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    solve();
 
     return 0;
 }
