ab8b0d256b117c78bd9b018ea34b3cdea3cc774a
woojinrnd <122770475+woojinrnd@users.noreply.github.com>
2025-06-22 20:10:23 +0900

    [Silver I] Title: 영역 구하기, Time: 0 ms, Memory: 2104 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.cc
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.cc
@@ -6,26 +6,24 @@
 using namespace std;
 
 int n, m, k;
-int N[102][102];
-int dist[102][102];
+int N[101][101];
+int dist[101][101];
 int dx[] = {1, 0, -1, 0};
 int dy[] = {0, 1, 0, -1};
-struct point {
+struct point { 
     int x, y;
 };
 int cnt;
-int area[100000];
+vector<int> each_area;
 
 void input() {
-    cin >> m >> n >> k;
-    while(k--) {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 0;
-        int y2 = 0;
+    cin >> n >> m >> k;
+    for (int i=0; i<k; ++i) {
+        int x1, y1, x2, y2;
         cin >> x1 >> y1 >> x2 >> y2;
-        int new_y1 = m - y2;
-        int new_y2 = m - y1;
+        
+        int new_y1 = n - y2;
+        int new_y2 = n - y1;
         for (int i=new_y1; i<new_y2; ++i) {
             for (int j=x1; j<x2; ++j) {
                 N[i][j] = 1;
@@ -35,42 +33,44 @@ void input() {
 }
 
 void solution() {
-    // BFS Start : (i, j)
-    // 탐색 구간 : N[i][j] = 0 || 방문x
-    for (int i=0; i<m; ++i) {
-        for (int j=0; j<n; ++j) {
-            if (N[i][j] == 1 || dist[i][j]>0) continue;
+    //탐색영역 : N[i][j] = 0
+    for (int i=0; i<n; ++i) {
+        for (int j=0; j<m; ++j) {
+            if (N[i][j] == 1 || dist[i][j]!=0) continue;
+            cnt++;
+            int area = 0;
             queue<point> q;
             dist[i][j] = 1;
             q.push({i, j});
-            cnt++;
             while(!q.empty()) {
                 point cur = q.front();
                 q.pop();
-                area[cnt]++;
-                for(int dir=0; dir<4; ++dir) {
+                area++;
+
+                for (int dir=0; dir<4; ++dir) {
                     int nx = cur.x + dx[dir];
                     int ny = cur.y + dy[dir];
-                    if (nx<0||nx>=m||ny<0||ny>=n) continue;
-                    if (N[nx][ny]==1||dist[nx][ny]>0)continue;
+                    if (nx<0||nx>=n||ny<0||ny>=m) continue;
+                    if (N[nx][ny]==1||dist[nx][ny]!=0) continue;
                     dist[nx][ny] = 1;
                     q.push({nx, ny});
                 }
             }
+            each_area.push_back(area);
+            // cout << area << ' ';
         }
     }
+    sort(each_area.begin(), each_area.end());
     cout << cnt << '\n';
-    sort(area+1, area+cnt+1);
-    for (int i=1; i<=cnt; ++i) {
-        cout << area[i] << ' ';
+    for (int i : each_area) {
+        cout << i << ' ';
     }
 }
 
 void show() {
-    for (int i=0; i<m; ++i) {
-        for (int j=0; j<n; ++j) {
+    for (int i=0; i<n; ++i) {
+        for (int j=0; j<m; ++j) {
             cout << N[i][j] << ' ';
-            // cout << dist[i][j] << ' ';
         }
         cout << '\n';
     }
