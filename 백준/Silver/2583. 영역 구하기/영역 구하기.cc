f547aac23a9149e1ca93d72c5eba92180b2c7416
woojinrnd <122770475+woojinrnd@users.noreply.github.com>
2025-08-26 21:28:04 +0900

    [Silver I] Title: 영역 구하기, Time: 0 ms, Memory: 2076 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.cc
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.cc
@@ -5,89 +5,91 @@
 
 using namespace std;
 
-int n, m, k;
-int N[101][101];
-int dist[101][101];
-int dx[] = {1, 0, -1, 0};
-int dy[] = {0, 1, 0, -1};
-struct point { 
-    int x, y;
+int N, M, K;
+int MAP[101][101];
+bool vist[101][101];
+int dx[] = { 1, 0, -1, 0 };
+int dy[] = { 0, 1, 0, -1 };
+struct point {
+	int x, y;
 };
-int cnt;
-vector<int> each_area;
+struct rec {
+	int x1, y1, x2, y2;
+};
+rec Rectangle[101];
+vector<int> area;
 
 void input() {
-    cin >> n >> m >> k;
-    for (int i=0; i<k; ++i) {
-        int x1, y1, x2, y2;
-        cin >> x1 >> y1 >> x2 >> y2;
-        
-        int new_y1 = n - y2;
-        int new_y2 = n - y1;
-        for (int i=new_y1; i<new_y2; ++i) {
-            for (int j=x1; j<x2; ++j) {
-                N[i][j] = 1;
-            }
-        }
-    }
+	cin >> N >> M >> K;
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < M; ++j) {
+			MAP[i][j] = 0;
+		}
+	}
+	for (int i = 0; i < K; ++i) {
+		int x1, y1, x2, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+		Rectangle[i] = { x1, y1, x2, y2 };
+	}
+}
+
+// 좌표 (x, y) -> (col, raw)
+// x는 그대로
+// y는 (N-y2) ~ (N~y1)
+
+void draw() {
+	for (int i = 0; i < K; ++i) {
+		for (int j = (N - Rectangle[i].y2); j < (N - Rectangle[i].y1); ++j) {
+			for (int k = Rectangle[i].x1; k < Rectangle[i].x2; ++k) {
+				MAP[j][k] = 1;
+			}
+		}
+	}
 }
 
 void solution() {
-    //탐색영역 : N[i][j] = 0
-    for (int i=0; i<n; ++i) {
-        for (int j=0; j<m; ++j) {
-            if (N[i][j] == 1 || dist[i][j]!=0) continue;
-            cnt++;
-            int area = 0;
-            queue<point> q;
-            dist[i][j] = 1;
-            q.push({i, j});
-            while(!q.empty()) {
-                point cur = q.front();
-                q.pop();
-                area++;
-
-                for (int dir=0; dir<4; ++dir) {
-                    int nx = cur.x + dx[dir];
-                    int ny = cur.y + dy[dir];
-                    if (nx<0||nx>=n||ny<0||ny>=m) continue;
-                    if (N[nx][ny]==1||dist[nx][ny]!=0) continue;
-                    dist[nx][ny] = 1;
-                    q.push({nx, ny});
-                }
-            }
-            each_area.push_back(area);
-            // cout << area << ' ';
-        }
-    }
-    sort(each_area.begin(), each_area.end());
-    cout << cnt << '\n';
-    for (int i : each_area) {
-        cout << i << ' ';
-    }
-}
-
-void show() {
-    for (int i=0; i<n; ++i) {
-        for (int j=0; j<m; ++j) {
-            cout << N[i][j] << ' ';
-        }
-        cout << '\n';
-    }
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < M; ++j) {
+			if (MAP[i][j] == 1 || vist[i][j]==true) continue;
+			queue<point> q;
+			vist[i][j] = true;
+			q.push({ i, j });
+			int each_area = 0;
+			while (!q.empty()) {
+				point cur = q.front();
+				q.pop();
+				each_area++;
+				for (int dir = 0; dir < 4; ++dir) {
+					int nx = cur.x + dx[dir];
+					int ny = cur.y + dy[dir];
+					if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+					if (MAP[nx][ny] == 1 || vist[nx][ny] == true) continue;
+					vist[nx][ny] = true;
+					q.push({ nx, ny });
+				}
+			}
+			area.push_back(each_area);
+		}
+	}
+	sort(area.begin(), area.end());
+	cout << area.size() << '\n';
+	for (int i : area) {
+		cout << i << ' ';
+	}
 }
 
 void solve() {
-    input();
-    solution();
-    // show();
+	input();
+	draw();
+	solution();
 }
 
 int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
-    solve();
+	solve();
 
-    return 0;
+	return 0;
 }
